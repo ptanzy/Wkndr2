abf5bd8ddbf3584c562e882e6b8606e76f83cb8b
ptanzy <ptanzy@comcast.net>
2019-04-23 21:27:34 -0400

    added modals signin/up to landing page and attempted to add firebase with env.local

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+
+//
+import Delay from 'react-delay';
+
+import Firebase from '../../firebase.js';
+//
+
 const $ = require("jquery");
+
 
 class Landing extends Component {
 
@@ -10,11 +18,62 @@ class Landing extends Component {
     $(document).foundation()
   }
 
+  signUp = event => {
+    event.preventDefault();
+    var userData = {
+        email: event.target.form[0].value,
+        password: event.target.form[1].value,
+        username: 0
+    };
+    Firebase.auth().createUserWithEmailAndPassword(userData.email, userData.password)
+    .then(function(user){
+        userData.username = user.user.uid;
+        localStorage.setItem('username', user.user.uid.toString());
+        $.post("/api/users", userData)
+        .then(function() {
+          getUsers();
+        });
+        
+    })
+    .catch(function(error){
+        alert(error);
+      });
+  
+    function getUsers() {
+      $.get("/api/users", function(data) {
+        var uname = localStorage.getItem("username");
+        for (var i=0; i < data.length; i++){
+          if (data[i].username == uname){
+            localStorage.setItem('userID', data[i].id.toString());
+            window.location.href = "/create";
+          }
+        }
+      });
+    }
+    
+  };
+
+  login = event => {
+    event.preventDefault();
+    var email = event.target.form[0].value;
+    var password = event.target.form[1].value;
+    Firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(user){
+        console.log(user);
+        localStorage.setItem('userID', user.user.uid);
+        window.location.href = "/create"; //redirects user to datasets page
+    })
+    .catch(function(error){
+        alert(error);
+    });
+    
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div class="entire">
-          <div class="hero-full-screen">
+        <div className="entire">
+          <div className="hero-full-screen">
 
             <div className="top-content-section"></div>
     
@@ -29,10 +88,52 @@ class Landing extends Component {
           </div>
 
           <div id="main-content-section" data-magellan-target="main-content-section">
-            <button className="button large">Sign-In</button>
-            <button className="button large">Sign-Up</button>
-            <h1>Enjoy Wkndr!</h1>
+            <div id="button-group">
+              <button href="#" className="button large" data-open="sign-in-modal">Login</button>
+              <button href="#" className="button large" data-open="sign-up-modal">Sign Up</button>
+              <h1>Enjoy Wkndr!</h1>
+            </div>
           </div>
+          
+
+          <div className="reveal sign-in-modal" id="sign-in-modal" data-reveal>
+            <form onSubmit={e => e.preventDefault()}>
+              <h3>Login to Wkndr</h3>
+              <div className="row columns">
+                <label>E-Mail
+                  <input type="text" name="keyword" placeholder="E-Mail"></input>
+                </label>
+              </div>
+              <div className="row columns">
+                <label>Password
+                  <input type="text" name="keyword" placeholder="Password"></input>
+                </label>
+              </div>
+              <button type="button" className="primary button expanded search-button" onClick={this.login}>
+                LOGIN
+              </button>
+            </form>
+          </div>
+
+          <div className="reveal sign-up-modal" id="sign-up-modal" data-reveal>
+            <form onSubmit={e => e.preventDefault()}>
+              <h3>Sign Up to Wkndr</h3>
+              <div className="row columns">
+                <label>E-Mail
+                  <input type="text" name="keyword" placeholder="Username"></input>
+                </label>
+              </div>
+              <div className="row columns">
+                <label>Password
+                  <input type="text" name="keyword" placeholder="Password"></input>
+                </label>
+              </div>
+              <button type="button" className="primary button expanded search-button" onClick={this.signUp}>
+                SIGN UP
+              </button>
+            </form>
+          </div>
+
         </div>
       </React.Fragment>
     );
